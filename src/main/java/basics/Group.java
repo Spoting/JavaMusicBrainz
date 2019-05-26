fe18b8a1154af914616d67c86df7e53bb8df179d
Spoting <ap.spiros00@gmail.com>
2019-05-26 07:29:37 +0300

    Hibernated Artist(Person,Group)

--- src/main/java/basics/Group.java
+++ src/main/java/basics/Group.java
@@ -1,56 +1,59 @@
 package basics;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+@Entity  
+@DiscriminatorValue("Group") 
 public class Group extends Artist {
+	
+    @OneToMany(targetEntity = Person.class, cascade=CascadeType.ALL)
+    @JoinColumn(name="gid")
+    private List<Person> members;
+    
+	@Override
+	public String toString() {
+		return "Group [members=" + members.size() + ", toString()=" + super.toString() + "]";
+	}
 
-    private String beginDate;
-    private String endDate;
-    private ArrayList<Person> members;
+	public Group() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    public Group() {
-        super();
-    }
 
-    public Group(String beginDate, String endDate, ArrayList<Person> members,
-            String id, String name, String country, String cities,
-            ArrayList<String> aliases, ArrayList<String> tags, String type) {
 
-        super(id, name, country, cities, aliases, tags, type);
-        this.beginDate = beginDate;
-        this.endDate = endDate;
-        this.members = members;
-    }
 
-    @Override
-    public String toString() {
-        return "Group [beginDate=" + beginDate + ", endDate=" + endDate + ", " + super.toString() + ",\n [" + "members=" + members + " "
-                + "]]\n";
-    }
+	public Group(String id, String name, String country, ArrayList<String> aliases, ArrayList<String> tags, String type,
+			String cities, Date birthDate, Date deathDate, ArrayList<Person> members) {
+		super(id, name, country, aliases, tags, type, cities, birthDate, deathDate);
+		this.members = members;
+		// TODO Auto-generated constructor stub
+	}
 
-    public String getBeginDate() {
-        return beginDate;
-    }
+	public Group(String id, String name) {
+		super(id, name);
+		// TODO Auto-generated constructor stub
+	}
 
-    public void setBeginDate(String beginDate) {
-        this.beginDate = beginDate;
-    }
+	public List<Person> getMembers() {
+		return members;
+	}
 
-    public String getEndDate() {
-        return endDate;
-    }
+	public void setMembers(List<Person> members) {
+		this.members = members;
+	}
+    
 
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
-    public ArrayList<Person> getMembers() {
-        return members;
-    }
-
-    public void setMembers(ArrayList<Person> members) {
-        this.members = members;
-    }
 
 }
